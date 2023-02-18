2606e9b8ea0b6498405d0d00e2c0d27da9d01747
rabikhan1 <108327450+rabikhan1@users.noreply.github.com>
2023-02-18 16:56:50 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,24 @@
 const path = require('path')
-const withImages = require('next-images');
-const withPlugins = require('next-compose-plugins');
+// const withImages = require('next-images');
+// const withPlugins = require('next-compose-plugins');
 
 module.exports = {
-    distDir: 'build',
+    // distDir: 'build',
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
     trailingSlash: true,
     optimizeFonts: false,
-}
-
-
-const nextSettings = {
     env: {
         title: 'Unique Associates',
         titleDescription: 'Client Panel',
     },
-};
+}
 
 
-module.exports = withPlugins([withImages(), nextSettings]);
+
+
+// module.exports = withPlugins([withImages(), nextSettings]);
