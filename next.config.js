8872116ad57898b95e17badbe6a31b9a4fa222ba
rabikhan1 <108327450+rabikhan1@users.noreply.github.com>
2023-02-18 16:24:14 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
     distDir: 'build',
     sassOptions: {
-        includePaths: [path.join('https://rabikhan1.github.io/unique-associates/', 'styles')],
+        includePaths: [path.join('https://rabikhan1.github.io/unique-associates/', 'app/styles')],
     },
     trailingSlash: true,
     optimizeFonts: false,
