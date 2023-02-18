175ebc7796602d86203956761363fd1adf7320f6
rabikhan1 <108327450+rabikhan1@users.noreply.github.com>
2023-02-18 16:29:44 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
 const path = require('path')
+const withImages = require('next-images');
 
 module.exports = {
     distDir: 'build',
     sassOptions: {
-        includePaths: [path.join('https://rabikhan1.github.io/unique-associates/', 'app/styles')],
+        includePaths: [path.join(__dirname, 'styles')],
     },
     trailingSlash: true,
     optimizeFonts: false,
 }
+
+
+const nextSettings = {
+    env: {
+        title: 'Unique Associates',
+        titleDescription: 'Client Panel',
+    },
+};
+
+
+module.exports = withPlugins([withImages(), nextSettings]);
